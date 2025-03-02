da2427541e6cca270813e9955a989438bb24baf7
OwaisPaul <152506988+OwaisPaul@users.noreply.github.com>
2025-03-02 11:10:51 +0530

    completed 1.2

--- src/App.jsx
+++ src/App.jsx
@@ -20,9 +20,11 @@ const App = () => {
     <div>
       <Header title={course} />
       <Content 
-      part1={part1} exercises1={exercises1}
+        part1={part1} exercises1={exercises1}
        part2={part2} exercises2={exercises2}
         part3={part3} exercises3 ={exercises3} />
+      
+      
         <Total allExercises ={exercises1 + exercises2 + exercises3} />
     </div>
   )
---
+++ src/Part.jsx
@@ -0,0 +1,9 @@
+const Part = (props) => {
+    return (
+        <p>
+            {props.part}: Exercises {props.Exercises}
+        </p>
+    )
+}
+
+export default Part
